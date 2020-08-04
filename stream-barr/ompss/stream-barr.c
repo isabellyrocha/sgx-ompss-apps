50db37cca1f7b3f4e1977a3fbd4ea148651818ad
isabellyrocha <isabelly.rocha@unine.ch>
2020-08-04 09:53:40 +0000

    Changing from constants to tunable variables.

--- stream-barr/ompss/stream-barr.c
+++ stream-barr/ompss/stream-barr.c
@@ -1,89 +1,12 @@
-/*-----------------------------------------------------------------------*/
-/* Program: Stream                                                       */
-/* Adapted to StarSs by Rosa M. Badia (Barcelona Supercomputing Center)	 */
-/* This version does not insert barriers after each set of operations,   */
-/* to promote task chaining in StarSs					 */
-/* Revision: $Id: stream.c,v 5.8 2007/02/19 23:57:39 mccalpin Exp mccalpin $ */
-/* Original code developed by John D. McCalpin                           */
-/* Programmers: John D. McCalpin                                         */
-/*              Joe R. Zagar                                             */
-/*                                                                       */
-/* This program measures memory transfer rates in MB/s for simple        */
-/* computational kernels coded in C.                                     */
-/*-----------------------------------------------------------------------*/
-/* Copyright 1991-2005: John D. McCalpin                                 */
-/*-----------------------------------------------------------------------*/
-/* License:                                                              */
-/*  1. You are free to use this program and/or to redistribute           */
-/*     this program.                                                     */
-/*  2. You are free to modify this program for your own use,             */
-/*     including commercial use, subject to the publication              */
-/*     restrictions in item 3.                                           */
-/*  3. You are free to publish results obtained from running this        */
-/*     program, or from works that you derive from this program,         */
-/*     with the following limitations:                                   */
-/*     3a. In order to be referred to as "STREAM benchmark results",     */
-/*         published results must be in conformance to the STREAM        */
-/*         Run Rules, (briefly reviewed below) published at              */
-/*         http://www.cs.virginia.edu/stream/ref.html                    */
-/*         and incorporated herein by reference.                         */
-/*         As the copyright holder, John McCalpin retains the            */
-/*         right to determine conformity with the Run Rules.             */
-/*     3b. Results based on modified source code or on runs not in       */
-/*         accordance with the STREAM Run Rules must be clearly          */
-/*         labelled whenever they are published.  Examples of            */
-/*         proper labelling include:                                     */
-/*         "tuned STREAM benchmark results"                              */
-/*         "based on a variant of the STREAM benchmark code"             */
-/*         Other comparable, clear and reasonable labelling is           */
-/*         acceptable.                                                   */
-/*     3c. Submission of results to the STREAM benchmark web site        */
-/*         is encouraged, but not required.                              */
-/*  4. Use of this program or creation of derived works based on this    */
-/*     program constitutes acceptance of these licensing restrictions.   */
-/*  5. Absolutely no warranty is expressed or implied.                   */
-/*-----------------------------------------------------------------------*/
 # include <stdio.h>
 # include <math.h>
 # include <float.h>
 # include <stdlib.h>
-//# include "limits.h"
 # include <sys/time.h>
 # include "omp.h"
 
-/* INSTRUCTIONS:
- *
- *	1) Stream requires a good bit of memory to run.  Adjust the
- *          value of 'N' (below) to give a 'timing calibration' of 
- *          at least 20 clock-ticks.  This will provide rate estimates
- *          that should be good to about 5% precision.
- */
-
-# define N	128*1024*1024 
 # define NTIMES	10
 # define OFFSET	0
-
-/*
- *	3) Compile the code with full optimization.  Many compilers
- *	   generate unreasonably bad code before the optimizer tightens
- *	   things up.  If the results are unreasonably good, on the
- *	   other hand, the optimizer might be too smart for me!
- *
- *         Try compiling with:
- *               cc -O stream_omp.c -o stream_omp
- *
- *         This is known to work on Cray, SGI, IBM, and Sun machines.
- *
- *
- *	4) Mail the results to mccalpin@cs.virginia.edu
- *	   Be sure to include:
- *		a) computer hardware model number and software revision
- *		b) the compiler flags
- *		c) all of the output from the test case.
- * Thanks!
- *
- */
-
 # define HLINE "-------------------------------------------------------------\n"
 
 # ifndef MIN
@@ -99,54 +22,29 @@
 //#define CellSs_tracing
 #define TUNED
 // Definition of BSIZE for CellSs 
-#ifdef CellSs
-#define BSIZE 4000
-#endif
+//#ifdef CellSs
+//#define BSIZE 4000
+//#endif
 // Definition of BSIZE for CellSs when using tracing
-#ifdef CellSs_tracing
-#define BSIZE 3200 
-#endif
-#ifdef SMPSs
+//#ifdef CellSs_tracing
+//#define BSIZE 3200 
+//#endif
+//#ifdef SMPSs
 // Definition of BSIZE for SMPSs
-#define BSIZE N/64
-#endif
-#ifdef OmpSs
+//#define BSIZE N/64
+//#endif
+//#ifdef OmpSs
 // Definitions of BSIZE for OmpSs
-#define BSIZE N/64
-#endif
-
-
-static double	__attribute__((aligned(4096)))a[N+OFFSET],
-		b[N+OFFSET],
-		c[N+OFFSET];
-
-static double	avgtime[4] = {0}, maxtime[4] = {0},
-		mintime[4] = {FLT_MAX,FLT_MAX,FLT_MAX,FLT_MAX};
-
-static char	*label[4] = {"Copy:      ", "Scale:     ",
-    "Add:       ", "Triad:     "};
-
-static double	bytes[4] = {
-    2 * sizeof(double) * N,
-    2 * sizeof(double) * N,
-    3 * sizeof(double) * N,
-    3 * sizeof(double) * N
-    };
+//#define BSIZE N/64
+//#endif
 
 extern double mysecond();
-extern void checkSTREAMresults();
-#ifdef TUNED
-extern void tuned_STREAM_Copy();
-extern void tuned_STREAM_Scale(double scalar);
-extern void tuned_STREAM_Add();
-extern void tuned_STREAM_Triad(double scalar);
-#endif
 
 #pragma omp task out ([bs]a, [bs]b, [bs]c)
 void init_task(double *a, double *b, double *c, int bs)
 {
 	int j;	
-	for (j=0; j < BSIZE; j++){
+	for (j=0; j < bs; j++){
 	        a[j] = 1.0;
 	        b[j] = 2.0;
 	        c[j] = 0.0;
@@ -154,23 +52,55 @@ void init_task(double *a, double *b, double *c, int bs)
   	}
 }
 
-void tuned_initialization()
+#pragma omp task in ([bs]a) out ([bs]c)
+void copy_task(double *a, double *c, int bs)
 {
-	int j;
-        for (j=0; j<N; j+=BSIZE)
-//Assumes N is multiple of BSIZE 
-            init_task (&a[j], &b[j], &c[j], BSIZE); 
+        int j;
+        for (j=0; j < bs; j++)
+                c[j] = a[j];
 }
 
-int
-main(int argc, char *argv[])
-    {
+#pragma omp task in ([bs]c ) out ([bs]b)
+void scale_task(double *b, double *c, double scalar, int bs)
+{
+        int j;
+        for (j=0; j < bs; j++)
+            b[j] = scalar*c[j];
+}
+
+#pragma omp task in ([bs]a, [bs]b) out ([bs]c)
+void add_task(double *a, double *b, double *c, int bs)
+{
+        int j;
+        for (j=0; j < bs; j++)
+           c[j] = a[j]+b[j];
+}
+
+#pragma omp task in ([bs]b, [bs]c) out ([bs]a)
+void triad_task(double *a, double *b, double *c, double scalar, int bs)
+{
+        int j;
+        for (j=0; j < bs; j++)
+            a[j] = b[j]+scalar*c[j];
+
+}
+
+void tuned_initialization(double a[], double b[], double c[], int N, int bs)
+{
+	int j;
+        for (j=0; j<N; j+=bs)
+            //Assumes N is multiple of BSIZE 
+            init_task(&a[j], &b[j], &c[j], bs); 
+}
+
+int main(int argc, char *argv[])
+{
     int			quantum, checktick();
     int			BytesPerWord;
     register int	j, k;
     double		scalar, t, times[4][NTIMES], total_time;
-
-
+    int                 N = atoi(argv[1])*1024*1024; //128*1024*1024;
+    int                 bs = N/atoi(argv[2]); //N/64; 
 
     /* --- SETUP --- determine precision and check timing --- */
 
@@ -194,7 +124,6 @@ main(int argc, char *argv[])
     printf ("Number of Threads = %i\n",k);
 #endif
 #ifdef SMPSs
-    printf(HLINE);
     printf("CSS_NUM_CPUS %s \n", getenv("CSS_NUM_CPUS"));
     k = atoi (getenv ("CSS_NUM_CPUS"));
     printf ("Number of CSS Threads = %i\n",k);
@@ -214,18 +143,28 @@ main(int argc, char *argv[])
 
     printf ("Printing one line per active thread....\n");
 
-    /* Get initial value for system clock. */
+    double   __attribute__((aligned(4096)))   *a = (double*) malloc(N*sizeof(double));
+    double   __attribute__((aligned(4096)))   *b = (double*) malloc(N*sizeof(double));
+    double   __attribute__((aligned(4096)))   *c = (double*) malloc(N*sizeof(double));
 
-/*
-    for (j=0; j<N; j++) {
-	a[j] = 1.0;
-	b[j] = 2.0;
-	c[j] = 0.0;
-	}
+    static double   avgtime[4] = {0}, maxtime[4] = {0},
+                    mintime[4] = {FLT_MAX,FLT_MAX,FLT_MAX,FLT_MAX};
 
-*/
-total_time = mysecond();
-    tuned_initialization();
+    static char     *label[4] = {"Copy:      ", "Scale:     ",
+                                 "Add:       ", "Triad:     "};
+
+    double   bytes[4] = {
+        2 * sizeof(double) * N,
+        2 * sizeof(double) * N,
+        3 * sizeof(double) * N,
+        3 * sizeof(double) * N
+    };
+
+    total_time = mysecond();
+    // tuned_initialization
+    for (j=0; j<N; j+=bs)
+        //Assumes N is multiple of BSIZE
+        init_task(&a[j], &b[j], &c[j], bs);
 #pragma omp taskwait
 
     /*	--- MAIN LOOP --- repeat test cases NTIMES times --- */
@@ -235,59 +174,66 @@ total_time = mysecond();
 	{
 	times[0][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Copy();
+     //  tuned_STREAM_Copy(a, c, N, BSIZE);
+    int j;
+    for (j=0; j<N; j+=bs)
+    // Assumes N is multiple of 100
+        copy_task(&a[j], &c[j], bs);
 #else
-printf("WARNING: This version is a port to StarSs that only works for TUNED option \n");
-	for (j=0; j<N; j++)
-	    c[j] = a[j];
+    printf("WARNING: This version is a port to StarSs that only works for TUNED option \n");
+    for (j=0; j<N; j++)
+        c[j] = a[j];
 #endif
 #pragma omp taskwait  
-	times[0][k] = mysecond() - times[0][k];
-	
-	times[1][k] = mysecond();
+    times[0][k] = mysecond() - times[0][k];
+    times[1][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Scale(scalar);
+    // tuned_STREAM_Scale(a, c, scalar, N, BSIZE);
+        //Assumes N is multiple of 100
+    for (j=0; j<N; j+=bs)
+         scale_task(&b[j], &c[j], scalar, bs);
 #else
-	for (j=0; j<N; j++)
-	    b[j] = scalar*c[j];
+    for (j=0; j<N; j++)
+        b[j] = scalar*c[j];
 #endif
 #pragma omp taskwait
-	times[1][k] = mysecond() - times[1][k];
-	
-	times[2][k] = mysecond();
+    times[1][k] = mysecond() - times[1][k];	
+    times[2][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Add();
+    // tuned_STREAM_Add(a, b, c, N, BSIZE);
+    // Assumes N is multiple of 100
+    for (j=0; j<N; j+=bs)
+        add_task(&a[j], &b[j], &c[j], bs);
 #else
 	for (j=0; j<N; j++)
 	    c[j] = a[j]+b[j];
 #endif
 #pragma omp taskwait 
-	times[2][k] = mysecond() - times[2][k];
-	
-	times[3][k] = mysecond();
+    times[2][k] = mysecond() - times[2][k];
+    times[3][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Triad(scalar);
+    //        tuned_STREAM_Triad(a, b, c, scalar, N, BSIZE);
+    //Assumes N is multiple of 100
+    for (j=0; j<N; j+=bs)
+        triad_task(&a[j], &b[j], &c[j], scalar, bs);
 #else
-	for (j=0; j<N; j++)
-	    a[j] = b[j]+scalar*c[j];
+    for (j=0; j<N; j++)
+        a[j] = b[j]+scalar*c[j];
 #endif
 #pragma omp  taskwait
-	times[3][k] = mysecond() - times[3][k];
-	}
-total_time = mysecond() - total_time;
+    times[3][k] = mysecond() - times[3][k];
+    }
+    total_time = mysecond() - total_time;
 #pragma omp taskwait
     /*	--- SUMMARY --- */
 
-    for (k=1; k<NTIMES; k++) /* note -- skip first iteration */
-	{
-	for (j=0; j<4; j++)
-	    {
+    for (k=1; k<NTIMES; k++) /* note -- skip first iteration */ {
+	for (j=0; j<4; j++) {
 	    avgtime[j] = avgtime[j] + times[j][k];
 	    mintime[j] = MIN(mintime[j], times[j][k]);
 	    maxtime[j] = MAX(maxtime[j], times[j][k]);
-	    }
 	}
-    
+    }
 
     printf("Function      Rate (MB/s)   Avg time     Min time     Max time\n");
     for (j=0; j<4; j++) {
@@ -304,7 +250,65 @@ total_time = mysecond() - total_time;
 
     printf("TOTAL time (including initialization) =  %11.4f seconds\n", total_time);
     /* --- Check Results --- */
-    checkSTREAMresults();
+
+    // checkSTREAMresults(a, b, c, BSIZE);
+    double aj,bj,cj;//,scalar;
+    double asum,bsum,csum;
+    double epsilon;
+    
+    /* reproduce initialization */
+    aj = 1.0;
+    bj = 2.0;
+    cj = 0.0;
+    /* a[] is modified during timing check */
+    aj = 2.0E0 * aj;
+    /* now execute timing loop */
+    scalar = 3.0;
+    for (k=0; k<NTIMES; k++) {
+        cj = aj;
+        bj = scalar*cj;
+        cj = aj+bj;
+        aj = bj+scalar*cj;
+    }
+    aj = aj * (double) (N);
+    bj = bj * (double) (N);
+    cj = cj * (double) (N);
+
+    asum = 0.0;
+    bsum = 0.0;
+    csum = 0.0;
+    for (j=0; j<N; j++) {
+	asum += a[j];
+	bsum += b[j];
+	csum += c[j];
+    }
+#ifdef VERBOSE
+    printf ("Results Comparison: \n");
+    printf ("        Expected  : %f %f %f \n",aj,bj,cj);
+    printf ("        Observed  : %f %f %f \n",asum,bsum,csum);
+#endif
+
+#ifndef abs
+#define abs(a) ((a) >= 0 ? (a) : -(a))
+#endif
+    epsilon = 1.e-8;
+
+    if (abs(aj-asum)/asum > epsilon) {
+	printf ("Failed Validation on array a[]\n");
+	printf ("        Expected  : %f \n",aj);
+	printf ("        Observed  : %f \n",asum);
+    } else if (abs(bj-bsum)/bsum > epsilon) {
+	printf ("Failed Validation on array b[]\n");
+	printf ("        Expected  : %f \n",bj);
+	printf ("        Observed  : %f \n",bsum);
+    } else if (abs(cj-csum)/csum > epsilon) {
+	printf ("Failed Validation on array c[]\n");
+	printf ("        Expected  : %f \n",cj);
+	printf ("        Observed  : %f \n",csum);
+    } else {
+        printf ("Solution Validates\n");
+    }
+
     printf(HLINE);
 
     return 0;
@@ -312,9 +316,8 @@ total_time = mysecond() - total_time;
 
 # define	M	20
 
-int
-checktick()
-    {
+int checktick()
+{
     int		i, minDelta, Delta;
     double	t1, t2, timesfound[M];
 
@@ -322,10 +325,9 @@ checktick()
 
     for (i = 0; i < M; i++) {
 	t1 = mysecond();
-	while( ((t2=mysecond()) - t1) < 1.0E-6 )
-	    ;
-	timesfound[i] = t1 = t2;
-	}
+	while( ((t2=mysecond()) - t1) < 1.0E-6 );
+  	timesfound[i] = t1 = t2;
+    }
 
 /*
  * Determine the minimum difference between these M values.
@@ -339,8 +341,8 @@ checktick()
 	minDelta = MIN(minDelta, MAX(Delta,0));
 	}
 
-   return(minDelta);
-    }
+    return(minDelta);
+}
 
 
 
@@ -358,7 +360,7 @@ double mysecond()
         return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
 }
 
-void checkSTREAMresults ()
+void checkSTREAMresults(double *a, double *b, double *c, int N)
 {
 	double aj,bj,cj,scalar;
 	double asum,bsum,csum;
@@ -422,68 +424,3 @@ void checkSTREAMresults ()
 		printf ("Solution Validates\n");
 	}
 }
-#pragma omp task in ([bs]a) out ([bs]c)
-void copy_task(double *a, double *c, int bs)
-{
-	int j;	
-	for (j=0; j < BSIZE; j++)
-		c[j] = a[j];
-}
-
-void tuned_STREAM_Copy()
-{
-	int j;
-        for (j=0; j<N; j+=BSIZE)
-//Assumes N is multiple of 100 
-            copy_task (&a[j], &c[j], BSIZE); 
-}
-
-#pragma omp task in ([bs]c ) out ([bs]b)
-void scale_task (double *b, double *c, double scalar, int bs)
-{
-	int j;	
-	for (j=0; j < BSIZE; j++)
-	    b[j] = scalar*c[j];
-}
-
-void tuned_STREAM_Scale(double scalar)
-{
-	int j;
-//Assumes N is multiple of 100 
-	for (j=0; j<N; j+=BSIZE)
-	       scale_task (&b[j], &c[j], scalar, BSIZE); 
-}
-
-#pragma omp task in ([bs]a, [bs]b) out ([bs]c)
-void add_task (double *a, double *b, double *c, int bs)
-{
-	int j;	
-	for (j=0; j < BSIZE; j++)
-	   c[j] = a[j]+b[j]; 
-}
-
-void tuned_STREAM_Add()
-{
-	int j;
-//Assumes N is multiple of 100 
-	for (j=0; j<N; j+=BSIZE)
-	    add_task(&a[j], &b[j], &c[j], BSIZE); 
-}
-
-#pragma omp task in ([bs]b, [bs]c) out ([bs]a)
-void triad_task (double *a, double *b, double *c, double scalar, int bs)
-{
-	int j;	
-	for (j=0; j < BSIZE; j++)
-            a[j] = b[j]+scalar*c[j];
-
-}
-
-void tuned_STREAM_Triad(double scalar)
-{
-	int j;
-//Assumes N is multiple of 100 
-	for (j=0; j<N; j+=BSIZE)
-	    triad_task (&a[j], &b[j], &c[j], scalar, BSIZE);
-}
-
