276b2b86f4979f96627b6420652257edcf534e29
isabellyrocha <isabelly.rocha@unine.ch>
2020-08-13 07:02:18 +0000

    Dot product update.

--- dot-product/ompss/dot-product.c
+++ dot-product/ompss/dot-product.c
@@ -63,8 +63,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    N = atol(argv[1]) * 1024L;
+    //N = atol(argv[1]) * 1024L;
     CHUNK_SIZE = atol(argv[2]) * 1024L;
+    N = atol(argv[1]) * CHUNK_SIZE * 1024L;
 
     double *A = malloc(N*sizeof(double));
     double *B = malloc(N*sizeof(double));
