b8a66e58ae76c3dcde14ff72daee4635c1a3552c
Isabelly Rocha <isabelly.rocha@unine.ch>
2020-11-18 11:28:21 +0100

    Update pdu_power.py

--- pdu_power.py
+++ pdu_power.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 # This is a parser for the json metrics returned by the web interface of the Lindy Ipower Control 2x6 XM
-# It writes the Voltage, Current, Active Power and Power factor to a CSV file
+# It writes the Voltage, Current, Active Power and Power factor to InfluxDB
 # usage:
-#       ./pdu-parser.py pdu-outputs-power.csv "http://example-pdu.url" 1
+#       python3 /home/ubuntu/pipetune/monitoring/pdu-power-parser.py http://example-pdu.url 1
 #       python3 /home/ubuntu/pipetune/monitoring/pdu-power-parser.py http://pdu-dell-1.cluster.iiun.unine.ch 1
-# hit Ctrl-C when you want to stop the capture
 
 import argparse
 import time
@@ -17,13 +16,13 @@ from influxdb import InfluxDBClient
 
 def get_nodename(node_id):
     switcher = {
-        0: "wedge",
+        0: "off",
         1: "cervino-6",
         2: "eiger-8",
         3: "eiger-6",
         4: "eiger-4",
         5: "eiger-7",
-        6: "wedge",
+        6: "off",
         7: "eiger-5",
         8: "eiger-3",
         9: "eiger-2",
@@ -49,9 +48,7 @@ def write_outputs_metrics_influxdb(timestamp, metrics_values, client):
     for output_id in range(2,11):
         nodename = get_nodename(output_id)
         power = metrics_values[output_id][4]['v']
-        
         json_body = get_json(nodename, timestamp, power)
-        print(json_body)
         client.write_points(json_body)    	
  
 def main(status_url, frequency):
@@ -62,9 +59,7 @@ def main(status_url, frequency):
             timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')	
             response = urllib.request.urlopen(status_url + '/statusjsn.js?components=16384')
             obj = response.readline().decode('utf8')
-#            print(obj)
             metrics = json.loads(obj)
-            print(metrics)
             write_outputs_metrics_influxdb(timestamp, metrics['sensor_values'][1]['values'], client)
             time.sleep(frequency)
     except KeyboardInterrupt:
